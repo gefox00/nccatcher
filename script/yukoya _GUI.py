62725c83f030ce91698c6b4758a11a791478bb29
gefox <genmaemell@gmail.com>
2023-02-16 12:04:40 +0900

    データベース機能追加

---
+++ script/yukoya _GUI.py
@@ -0,0 +1,27 @@
+import PySimpleGUI as Sg
+import pyperclip
+from yukoya import ykch_converter
+
+layout = [[Sg.Input(key='URL'), Sg.Button(button_text='変換', key='CONVERT')],
+          []
+          ]
+
+
+window = Sg.Window(title='', layout=layout)
+
+end_flag = True
+
+while end_flag:
+    event, value = window.read()
+
+    match event:
+        case 'CONVERT':
+            conv_data = ykch_converter(window['URL'].get()).cocoforia()
+            pyperclip.copy(conv_data)
+
+        case Sg.WIN_CLOSED:
+            end_flag = False
+            break
+        case _:
+            end_flag = False
+            break
---
+++ script/yukoya.py
@@ -0,0 +1,63 @@
+import requests
+
+class ykch_converter:
+    target = ''
+    def __init__(self, url):
+        self.target = url
+
+    def cocoforia(self):
+        # サーバーからデータを取得
+        target_url = self.target + '.js'
+        # データをJsonで確保
+        res = requests.get(target_url).json()
+        # データ組み立て用変数のセット
+        memo = ''
+        command = ''
+        param = []
+        out_data = {"kind": "character"}
+        status_data = []
+        # パレット生成とパラメータ生成
+        status_data.append({'label': f'おもい', 'value': 0, 'max': 0})
+        status_data.append({'label': f'ふしぎ', 'value': 0, 'max': 0})
+        param.append({'label': 'へんげ', 'value': res['NC1']})
+        param.append({'label': 'けもの', 'value': res['NC2']})
+        param.append({'label': 'おとな', 'value': res['NC3']})
+        param.append({'label': 'こども', 'value': res['NC4']})
+
+        for i in res['tunagarid_dst']:
+            status_data.append({'label': f'{i}へのゆめ', 'value': 0, 'max': 0})
+            command += f':{i}へのゆめ+1\n'
+            command += f'現在の{i}へのゆめ' + '{' + f'{i}へのゆめ' + '}\n'
+            command += f':{i}へのゆめ=0\n'
+            command += f':{i}へのつながり+1\n'
+            param.append({'label': f'{i}への感情', 'value': '受容'})
+
+        for i in res['tunagari_dst']:
+            status_data.append({'label': f'{i}へのゆめ', 'value': 0, 'max': 0})
+            command += f':{i}へのゆめ+1\n'
+            command += f'現在の{i}へのゆめ' + '{' + f'{i}へのゆめ' + '}\n'
+            command += f':{i}へのゆめ=0\n'
+
+        for ch, emo in zip(res['tunagari_dst'], res['tunagarid_name'][1:]):
+            param.append({'label': f'{ch}への感情', 'value': emo})
+
+
+        for cat, flv, name, cost in zip(res['Powers_lv'], res['Powers_kouka'], res['Powers_name'], res['Powers_koukatime']):
+            print(cost)
+            command += cat + ' ' + name + '：{' + name + '}\n'
+            param.append({'label': name, 'value': f'コスト{cost}：{flv}'})
+            memo += f'{cat}：{name}：{cost}\n{flv}\n\n'
+
+        status_data.append({'label': '町へのつながり', 'value': 2, 'max': 5})
+
+        for i in res['tunagari_dst']:
+            match i:
+                case _:
+                    status_data.append({'label': f'{i}へのつながり', 'value': 2, 'max': 5})
+
+        temp = {'name': res['pc_name'], 'initiative': 0,
+                'externalUrl': str(target_url), 'memo': memo, 'commands': command,
+                'status': status_data, 'params': param}
+        out_data['data'] = temp
+        str_out = str(out_data).replace('\'', '"')
+        return str_out
