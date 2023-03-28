b199849b9edf1654ee7ab4e3d43720146b7fc2f4
gefox00 <72073413+gefox00@users.noreply.github.com>
2023-03-28 18:02:14 +0900

    データベース機能追加

---
+++ script/cocofolia_clip_api.py
@@ -0,0 +1,57 @@
+import json
+
+
+class ClipApi:
+    def __init__(self):
+        self.data = {
+            "kind": "character",
+            "data": {
+                "name": "no-name",
+                "memo": "",
+                "initiative": 0,
+                "status": [],
+                "params": [],
+                "active": True,
+                "secret": False,
+                "invisible": False,
+                "hideStatus": False,
+                "color": "#ffffff",
+                "commands": "",
+            }
+        }
+
+    def set_name(self, name: str):
+        self.data['data']['name'] = name
+
+    def set_memo(self, memo: str):
+        self.data['data']['memo'] = memo
+
+    def set_initiative(self, inis: int):
+        self.data['data']['initiative'] = inis
+
+    def set_status(self, status: list):
+        self.data['data']['status'] = status
+
+    def set_params(self, params: list):
+        self.data['data']['params'] = params
+
+    def set_commands(self, commands: str):
+        self.data['data']['commands'] = commands
+
+    def set_secret(self, secret: bool):
+        self.data['data']['secret'] = secret
+
+    def set_invisible(self, invisible: bool):
+        self.data['data']['invisible'] = invisible
+
+    def set_hide_status(self, hide_status: bool):
+        self.data['data']['hideStatus'] = hide_status
+
+    def set_color(self, color: str):
+        self.data['data']['color'] = color
+
+    def txt_out(self):
+        return json.dumps(self.data, indent=4)
+
+
+print(ClipApi().txt_out())
--- script/joke2.py
+++ script/joke2.py
@@ -2,38 +2,9 @@ import PySimpleGUI as Sg
 import pyperclip
 import json
 import cocofolia_clip_api
-
-def load_region():
-
-
-def make_region():
-    # チャパレ初期化と組み立て
-    command_sauce = '-----ダイス-----\nchoice[PC1, PC2, PC3, PC4]\n:ユニット数+\n:ユニット数-\n:悪意\n'
-    for i in range(3):
-        command_sauce += f'NA+{i + 1}'
-    for i in range(3):
-        command_sauce += f'NA-{i + 1}\n-----マニューバ-----\n'
-    # 入力したマニューバをチャパレ用文字列に組み換え
-    for i in window['MN_table'].get():
-        command_sauce += f'{i[0]}《{i[1]}:{i[2]}:{i[3]}》{i[4]}\n'
-    # GUIで入力したココフォリア上で値の操作が必要な項目をステータスに変換
-    status = [{'label': 'ユニット数', 'value': int(window['main_unit'].get()),
-               'max': int(window['main_unit'].get())}
-              ]
-    # GUIで入力した固定値をパラメータに変換
-    params = [{'label': '悪意', 'value': int(window['main_EXP'].get())}]
-    clip_data.set_commands(command_sauce)
-    clip_data.set_params(params)
-    clip_data.set_status(status)
-    clip_data.set_name(window['main_name'].get())
-    clip_data.set_initiative(int(window['main_AP'].get()))
-
-
 clip_data = cocofolia_clip_api.ClipApi()
 M_timing = ['Au', 'Ac', 'Ra', 'Ju', 'Da']
 M_range = [str(i) for i in range(5)]
-for i in [f"{i}-{j}" for i in range(4) for j in range(i+1, 5)]:
-    M_range.append(i)
 M_range.append('自身')
 M_range.append('効果参照')
 Sg.theme('DarkBlue3')
@@ -42,7 +13,7 @@ layout = [[Sg.Menu(menu_items, key='file_menu')],
           [Sg.Text('駒名称'), Sg.Input(key='main_name', size=57)],
           [Sg.Text('悪意'), Sg.Input(key='main_EXP', size=5),
            Sg.Text('行動値'), Sg.Input(key='main_AP', size=5),
-           Sg.Text('ユニット数'), Sg.Input(key='main_unit', size=5)],
+           ],
           [Sg.Text('マニューバ編集')],
           [Sg.Text('マニューバ名称'), Sg.Input(key='M_name', size=49)],
           [Sg.Text('タイミング'), Sg.Combo(values=M_timing, size=4, key='M_timing'),
@@ -50,16 +21,17 @@ layout = [[Sg.Menu(menu_items, key='file_menu')],
            Sg.Text('射程'), Sg.Combo(values=M_range, key='M_range', size=8)],
           [Sg.Text('効\n果\n内\n容'), Sg.MLine(key='M_effect', size=(60, 10))],
           [Sg.Button(button_text='マニューバ登録', size=58, key='resist_data')],
-          [Sg.Text('登録マニューバ')],
+          [Sg.Text('登録マニューバ数:0', key='MN_reg')],
           [Sg.Table(headings=['名称', 'タイミング', 'コスト', '射程', '効果'],
                     auto_size_columns=False, values=[],
-                    size=(48, 10), key='MN_table',
+                    size=(48, 10),  justification='center',
+                    key='MN_table', max_col_width=20,
                     background_color='white', text_color='black',
                     alternating_row_color='skyblue')
            ],
           [Sg.Button(button_text='駒出力', size=58, key='output')]
           ]
-window = Sg.Window('Rエディタ', layout)
+window = Sg.Window('Hエディタ', layout)
 end_bol = True
 while end_bol:
     event, values = window.read()
@@ -81,9 +53,29 @@ while end_bol:
                            mn_data['effect']
                            ])
             window['MN_table'].update(tables)
+            window['MN_reg'].Update(f'登録マニューバ数:{len(window["MN_table"].get())}')
+
         case 'output':
-            make_region()
+            command_sauce = '-----ダイス-----\nchoice[PC1, PC2, PC3, PC4]\n:ユニット数+\n:ユニット数-\n:悪意\n'
+            for i in range(3):
+                command_sauce += f'NA+{i + 1}'
+            for i in range(3):
+                command_sauce += f'NA-{i + 1}\n-----マニューバ-----\n'
+            for i in window['MN_table'].get():
+                command_sauce += f'{i[0]}《{i[1]}:{i[2]}:{i[3]}》{i[4]}\n'
+            status = [{'label': 'パーツ数', 'value': len(window['MN_table'].get()),
+                       'max': len(window['MN_table'].get())}
+                      ]
+            params = [{'label': '悪意', 'value': int(window['main_EXP'].get())}]
+            clip_data.set_commands(command_sauce)
+            clip_data.set_params(params)
+            clip_data.set_status(status)
+            clip_data.set_name(window['main_name'].get())
+            clip_data.set_initiative(int(window['main_AP'].get()))
             pyperclip.copy(clip_data.txt_out())
+            print(len(window['MN_table'].get()))
+
+
         case 'ファイルから開く':
             make_data = 0
             target = ['main_name', 'main_EXP', 'main_AP', 'main_unit',
@@ -99,12 +91,16 @@ while end_bol:
                 data = json.load(r)
 
         case 'ファイルに保存':
+            target = ['main_name', 'main_EXP', 'main_AP', 'main_unit',
+                      'M_name', 'M_timing', 'M_cost', 'M_range', 'M_effect', 'MN_table']
             save_file = Sg.popup_get_file("保存するファイル名を入力してください",
                                           save_as=True,
                                           file_types=(("Json", "*.json"),))
-            with open(save_file, 'w', encoding='utf8')as w:
-                make_region()
-                w.write(clip_data.txt_out())
+
+            for i in target:
+                window[i].get()
+
+
         case _:
             end_bol = False
             break
